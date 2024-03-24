ca0ad507766a0e22ed5b00b7cec2e83b3af7d94c
Attrup <33235395+Attrup@users.noreply.github.com>
2024-03-24 14:36:58 +0100

    Updated StatTracker binary to use new eframe call

--- src/bin/stattracker.rs
+++ src/bin/stattracker.rs
@@ -1,23 +1,26 @@
 #![allow(non_snake_case)]
 #![windows_subsystem = "windows"]
 
-use eframe::egui::{Style, Visuals};
+use eframe::egui::{Style, Vec2, ViewportBuilder, Visuals};
 use stattracker::app::gui::GUI;
 
 // Set initial window size
-//const WIDTH: f32 = 270.0;
-// const HEIGHT: f32 = 330.0;
+const WIDTH: f32 = 270.0;
+const HEIGHT: f32 = 330.0;
 
 /// Run the application
-fn main() -> eframe::Result<()> {
+fn main() -> eframe::Result<(), eframe::Error> {
+    // Set up the window for the application
     eframe::run_native(
         "Hitman StatTracker",
         eframe::NativeOptions {
-            //min_window_size: Some(egui::Vec2::new(WIDTH, HEIGHT)),
-            //initial_window_size: Some(egui::Vec2::new(WIDTH, HEIGHT)),
+            viewport: ViewportBuilder::default()
+                .with_inner_size(Vec2::new(WIDTH, HEIGHT))
+                .with_min_inner_size(Vec2::new(WIDTH, HEIGHT)),
             run_and_return: false,
-            ..eframe::NativeOptions::default()
+            ..Default::default()
         },
+        // Initialize the GUI
         Box::new(|cc| {
             let style = Style {
                 visuals: Visuals::light(),
